bffdf70722f5475f5f8c64a46a7e41fd1b7fab36
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-05-15 22:59:58 -0700

    added element IDs to app.js and html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
-
+//lint names undefined warning killers
+var document;
 // Objects will go here
 var store = {
   strName: '1st and Pike',
@@ -7,5 +8,11 @@ var store = {
   maxCust: 65,
   avgCookiePerSale: 6.3
 };
-var eleStore = document.elementById('storeName');
-elestore.textContent = store.strName;
+var eleStore = document.getElementById('strName');
+eleStore.textContent = store.strName;
+var eleMin = document.getElementById('minCust');
+eleMin.textContent = store.minCust;
+var eleMax= document.getElementById('maxCust');
+eleMax.textContent = store.maxCust;
+var eleAvg = document.getElementById('avgCookiePerSale');
+eleAvg.textContent = store.avgCookiePerSale;
