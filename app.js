040cfe2084ced3a90f209dadd02ce386e851af05
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-05-15 20:13:45 -0700

    added first store object, no methods

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Objects will go here
+var store = {
+  strName: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerSale: 6.3
+};
+var eleStore = document.elementById('storeName');
+elestore.textContent = store.strName;
