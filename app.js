49294938a0fe65000cb19a278696d820b8684aad
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-05-17 04:10:18 -0700

    fixed capitol hill not showing up in tablesalso added table header creator

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var capitolHillTR = document.getElementById ('capitolHill');
 var alkiTR = document.getElementById ('alki');
 
 //hours table array
-var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm' ]
+var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm' ];
 
 // Constructor will go here
 function CookieStore (tableElement, name, minCust, maxCust, avgCookiesPerSale) {
@@ -36,7 +36,7 @@ CookieStore.prototype.ranCustNum = function () {
   return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
 };
 
-//random cookie sales generator
+//random cookie sales generator 1st and pike is REALLY busy!
 CookieStore.prototype.hourlyCookies = function () {
   for (var i = 0; i < hoursOpen.length; i++) {
     var totalHourlyCookies = Math.floor(this.avgCookiesPerSale * this.ranCustNum());
@@ -63,7 +63,34 @@ CookieStore.prototype.render = function() {
   var newTable = document.getElementById('sales_report');
   newTable.appendChild(this.tableElement);
 };
+//create a new table
+function tableAdd() {
+  var newElement;
+  for (var i = 0; i < 16; i++) {
+    newElement = document.createElement('tr');
+    newElement.textContent = this.salesResults[i];
+    this.tableElement.appendChild(newElement);
+  }
+}
 
+// tableAdd();
+
+//create table header
+function tableHeaderAdd() {
+  var hoursOpenCell;
+  var addRow = document.createElement('tr');
+  var addHead = document.createElement('th');
+  addRow.appendChild(addHead);
+  for (var i = 0; i < 15; i++) {
+    hoursOpenCell = document.createElement('th');
+    hoursOpenCell.textContent = hoursOpen[i];
+    addRow.appendChild(hoursOpenCell);
+  }
+  var newTable = document.getElementById('sales_report');
+  newTable.appendChild(addRow);
+}
+//calls the tableHeaderAdd function
+tableHeaderAdd();
 //cookie stores and their data and random cookies calls
 var firstAndPike = new CookieStore (firstAndPikeTR, '1st and Pike', 23, 65, 6.3);
 firstAndPike.hourlyCookies();
@@ -78,6 +105,9 @@ seattleCenter.hourlyCookies();
 seattleCenter.render();
 
 var capitolHill = new CookieStore (capitolHillTR, 'Capitol Hill', 20, 38, 2.3);
+capitolHill.hourlyCookies();
+capitolHill.render();
+
 var alki = new CookieStore (alkiTR, 'Alki', 2, 16, 4.6);
 alki.hourlyCookies();
 alki.render();
